8f4d74fd5a5d709838ff48764337bce8f432842f
Thorsten Seyschab <business@todde.tv>
2025-01-23 14:50:24 +0100

    feat: add unplugin starter content with src, scripts & playground

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import type { UnpluginFactory } from 'unplugin'
+import type { Options } from './types'
+import { createUnplugin } from 'unplugin'
+
+export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
+  name: 'unplugin-starter',
+  transform(code) {
+    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
+  },
+  transformInclude(id) {
+    return id.endsWith('main.ts')
+  },
+})
+
+export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
+
+export default unplugin
---
+++ src/nuxt.ts
@@ -0,0 +1,25 @@
+import type { Options } from './types'
+import { addVitePlugin, addWebpackPlugin, defineNuxtModule } from '@nuxt/kit'
+import vite from './vite.js'
+import webpack from './webpack.js'
+import '@nuxt/schema'
+
+export interface ModuleOptions extends Options {
+
+}
+
+export default defineNuxtModule<ModuleOptions>({
+  defaults: {
+    // ...default options
+  },
+  meta: {
+    configKey: 'unpluginStarter',
+    name: 'nuxt-unplugin-starter',
+  },
+  setup(options, _nuxt) {
+    addVitePlugin(() => vite(options))
+    addWebpackPlugin(() => webpack(options))
+
+    // ...
+  },
+})
