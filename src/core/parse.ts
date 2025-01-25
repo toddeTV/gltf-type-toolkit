e314b1fdd4fee3fe5ca9137f7ecbd62f0c3cbc2e
Andreas Fehn <fehnomenal@fehn.systems>
2025-01-25 20:34:21 +0100

    give json string to the gltfloader

--- src/core/parse.ts
+++ src/core/parse.ts
@@ -16,7 +16,7 @@ export async function parseGltfModel(gltf: GLTFLoader, modelFile: string): Promi
     throw new Error(`"${modelFile}" is not a file!`)
   }
 
-  const json = (await readFile(modelFile)).buffer
+  const json = (await readFile(modelFile, { encoding: 'utf8' }))
 
   const model = await gltf.parseAsync(json, dirname(modelFile))
 
