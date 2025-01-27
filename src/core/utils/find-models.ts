09ee9a2c580de24b724aa72a701dfbf14e3e1f96
Andreas Fehn <fehnomenal@fehn.systems>
2025-01-28 00:12:22 +0100

    unwrap unnecessary await

--- src/core/utils/find-models.ts
+++ src/core/utils/find-models.ts
@@ -5,9 +5,7 @@ import { GLTF_MODEL_EXTENSIONS } from '../constants.js'
 export async function findAllModelsInDir(dir: string): Promise<string[]> {
   const entries = await readdir(dir, { recursive: true, withFileTypes: true })
 
-  return await Promise.all(
-    entries
-      .filter(e => GLTF_MODEL_EXTENSIONS.some(ext => e.name.endsWith(ext)))
-      .map(async entry => join(entry.parentPath, entry.name)),
-  )
+  return entries
+    .filter(e => GLTF_MODEL_EXTENSIONS.some(ext => e.name.endsWith(ext)))
+    .map(entry => join(entry.parentPath, entry.name))
 }
